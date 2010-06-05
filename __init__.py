672cdb2d6261737dff912e32631e299a36ab5139
Laurent Claessens <moky.math@gmail.com>
2010-06-05 06:54:52 +0200

    Begins development of PytexTools 	new file:   PytexTools.py 	modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 
 ###########################################################################
-#	This is LaTeXparser.py
+#	This is the package LaTeXparser
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
