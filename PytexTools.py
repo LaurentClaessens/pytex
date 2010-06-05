9702f464cf17870da1c3c0c40cae207ea1dbb5fd
Laurent Claessens <moky.math@gmail.com>
2010-06-05 19:33:24 +0200

    The class Compilation passes from LaTeXparser to LaTeXparser.PytexTools. 	modified:   PytexTools.py 	modified:   __init__.py

--- PytexTools.py
+++ PytexTools.py
@@ -28,6 +28,66 @@ pytex is a non-yet published pre-compilation system. Don't try to understand wha
 
 import LaTeXparser
 
+class Compilation(object):
+	"""
+	Launch the compilation of a document in various ways.
+	Takes a filename as argument
+
+	Usage examples
+	X=LaTeXparser.Compilation("MyLaTeXFile.tex")	# Creates the Compilation object
+	X.bibtex()					# Apply bibtex
+	X.chain_dvi_ps_pdf()				# Produce the pdf file
+	"""
+	def __init__(self,filename):
+		self.filename=filename
+		self.generic_filename = self.filename[:self.filename.rindex(".")]
+	def bibtex(self):
+		commande_e="bibtex "+self.generic_filename
+		os.system(commande_e)
+	def makeindex(self):
+		commande_e="makeindex "+self.generic_filename
+		os.system(commande_e)
+	def nomenclature(self):
+		commande_e="makeindex -s nomencl.ist -o "+self.generic_filename+".nls "+self.generic_filename+".nlo"		
+		os.system(commande_e)
+	def special_stuffs(self):
+		self.bibtex()
+		self.makeindex()
+		self.nomenclature()
+	def latex(self):
+		"""Produce a dvi file"""
+		commande_e="/usr/bin/latex --src-specials "+self.filename
+		os.system(commande_e)
+	def chain_dvi_ps(self,papertype="a4"):
+		"""
+		The chain tex->div->ps
+
+		After compiling by self.latex(), produce a ps file from the dvi by the command
+		dvips -t <papertype> <self.filename>.dvi
+		
+		Optional parameter : papertype is "a4" by default
+		"""
+		self.latex()
+		commande_e="dvips -t %s %s.dvi"%(papertype,self.generic_filename)
+		os.system(commande_e)
+	def chain_dvi_ps_pdf(self,papertype="a4"):
+		"""
+		The chain tex->dvi-ps->pdf 
+		This is more or less the only way to produce a pdf file containing pstricsk figures and hyperref links.
+
+		After having produced the ps by self.chain_dvi_ps(), produce a pdf file from the ps by the command
+		ps2pdf <self.filename>.ps
+		
+		Optional parameter : papertype is "a4" by default (to be passed to dvips)
+		"""
+		self.chain_dvi_ps(papertype)
+		commande_e="ps2pdf "+self.generic_filename+".ps" 
+		os.system(commande_e)
+	def latex_more(self):
+		self.special_stuffs()
+		self.latex()
+		self.special_stuffs()
+
 class CodeBox(dict):
 	"""
 	This class is intended to keep some portions of LaTeX code in a fresh box, allowing to retrieve them later.
