521df43e299d1394210381c13746db8fb5f0010e
Laurent Claessens <moky.math@gmail.com>
2010-07-22 11:12:17 +0200

    Typo. 	modified:   PytexTools.py

--- PytexTools.py
+++ PytexTools.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 
 ###########################################################################
-#	This is part of the the package LaTeXparser
+#	This is part of the package LaTeXparser
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
