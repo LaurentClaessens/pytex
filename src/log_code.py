5da252e53fa2d2d70f55cd7b169b47745658373b
Laurent Claessens <laurent@claessens-donadello.eu>
2020-02-07 06:55:00 +0100

    (log file) Fix a crash happening when we analyse the message "I moved some lines to the next page".

--- src/log_code.py
+++ src/log_code.py
@@ -93,9 +93,14 @@ class LogCode:
             genre = mots[0]
             label = mots[1][1:-1]
             try:
+                dprint("mots", mots)
                 page = mots[mots.index("page")+1]
             except ValueError:
                 page = -1
+            except IndexError:
+                # There is a warning 
+                # "I moved some lines to the next page."
+                continue
             if genre == "Reference":
                 labels = [w.label
                           for w in self.undefined_references]
